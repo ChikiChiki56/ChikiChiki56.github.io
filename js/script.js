096711543023168adb091f00169772faa691f3c6
ChikiChiki56 <zhi_rider@hotmail.com>
2024-03-13 18:41:55 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', () => {
+    let scrollDictance = window.scrollY; 
+    document.querySelectorAll('.section').forEach((el, i) => {
+        if (el.offsetTop - document.querySelector ('.nav').clientHeight <= scrollDictance) {
+            document.querySelectorAll('.nav a').forEach((el) => {
+                if (el.classList.contains('active')) {
+                    el.classList.remove('active'); 
+                }
+            });
+            document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
+        }
+    });
+});
